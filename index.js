50b3a2b513731ec2f7df356b091a99849f7fe983
Mark <125941419+Rondepart@users.noreply.github.com>
2026-07-31 21:29:18 +0800

    Add new function to add book to library which generates its own bookid, takes book details as param and pushes the book to the library array

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const myLibrary = [];
 
-function Book(title, author, numOfPages, haveRead) {
+function Book(bookId,title, author, numOfPages, haveRead) {
+    this.bookId = bookId
     this.title = title;
     this.author = author;
     this.numOfPages = numOfPages;
     this.haveRead = haveRead;
+}
+
+function addBookToLibrary(title, author, numOfPages, haveRead) {
+    const bookId = crypto.randomUUID();
+    const newBook = new Book(bookId, title, author, numOfPages, haveRead);
+    myLibrary.push(newBook);
 }
