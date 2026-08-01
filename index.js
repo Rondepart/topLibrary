2d9ac7a47c484f69c8a223c631bdb3ddc8c67b85
Mark <125941419+Rondepart@users.noreply.github.com>
2026-08-01 19:46:58 +0800

    Add delete button using DOM, attach an event listener to main wrapper for delete button action. Set book card css properties for better item layout

--- index.js
+++ index.js
@@ -20,11 +20,19 @@ function displayLibraryBooks() {
     mainWrapper.innerHTML = '';
 
     myLibrary.forEach(book => {
-        const bookEntryWrapper = document.createElement('div');
-        bookEntryWrapper.classList.add('book-card');
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        bookCard.dataset.bookId = book.bookId;
 
         const bookInfo = document.createElement('div');
         bookInfo.classList.add('book-card-info');
+
+        const cardButtonWrapper = document.createElement('div');
+        cardButtonWrapper.classList.add('book-card-btn-wrapper');
+
+        const cardButtonDelete = document.createElement('button');
+        cardButtonDelete.classList.add('book-card-delete');
+        cardButtonDelete.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="18" width="18"><title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>`;
 
         const title = document.createElement('p');
         title.classList.add('book-card-title');
@@ -46,9 +54,11 @@ function displayLibraryBooks() {
         bookInfo.appendChild(author);
         bookInfo.appendChild(pages);
         bookInfo.appendChild(status);
-
-        bookEntryWrapper.appendChild(bookInfo);
-        mainWrapper.appendChild(bookEntryWrapper);
+        cardButtonWrapper.appendChild(cardButtonDelete);
+        
+        bookCard.appendChild(bookInfo);
+        bookCard.appendChild(cardButtonWrapper)
+        mainWrapper.appendChild(bookCard);
     });
 }
 
@@ -68,3 +78,15 @@ addBookBtn.addEventListener('click', () => {
     document.getElementById('newBookForm').reset();
     document.getElementById('newBook').close();
 })
+
+mainWrapper.addEventListener('click', (e) => {
+    const deleteBtn = e.target.closest('.book-card-delete');
+    if (!deleteBtn) return;
+
+    const bookCard = deleteBtn.closest('.book-card');
+    const bookId = bookCard.dataset.bookId;
+
+    const index = myLibrary.findIndex(b => b.bookId === bookId);
+    myLibrary.splice(index, 1);
+    displayLibraryBooks();
+});
